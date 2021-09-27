44ddf2b5bb4579e8c53a366fbfa888e0aca41fce
yeramii <ramit.noh@gmail.com>
2021-09-28 02:13:42 +0900

    Fix 1966 s1

--- 백준/1966_프린터큐/s1.py
+++ 백준/1966_프린터큐/s1.py
@@ -8,7 +8,7 @@ for tc in range(1, T + 1):
 
     cnt = 0
     front = 0
-    rear = (N - front - 1) % N
+    rear = N - 1
     while True:
         m_value = max(points)
         if max(points[front:]) == m_value:
@@ -17,21 +17,18 @@ for tc in range(1, T + 1):
             m_idx = points.index(m_value)
         cnt += 1
 
+        points.pop(m_idx)
+        N -= 1
         if m_idx == M:
             break
         elif m_idx > M:
-            points.pop(m_idx)
-            N -= 1
             if m_idx == N:
                 front = 0
             else:
                 front = m_idx
-            rear = (N + front - 1) % N
         else:
-            points.pop(m_idx)
-            N -= 1
             front = m_idx
             M -= 1
-            rear = (N + front - 1) % N
+        rear = (N + front - 1) % N
 
     print(cnt)
